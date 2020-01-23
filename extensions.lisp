abfb1763c69d71ddd8e65525d65344b09cf7b949
ava <dev@computerfox.xyz>
2020-01-23 16:23:36 -0500

    added thin-wrapper around tooter:make-status
    fixed reply function to use proper tooter functions

--- extensions.lisp
+++ extensions.lisp
@@ -8,11 +8,11 @@
   "regex to check for the NoBot tag")
 
 (defmethod no-bot-p ((id integer))
-  "checks an account's boi and profile fields to see if they contain a NoBot tag"
+  "checks an account's bio and profile fields to see if they contain a NoBot tag"
   (no-bot-p (tooter:find-account (bot-client *bot*) id)))
 
 (defmethod no-bot-p ((account tooter:account))
-  "checks an account's boi and profile fields to see if they contain a NoBot tag"
+  "checks an account's bio and profile fields to see if they contain a NoBot tag"
   (or (cl-ppcre:scan *no-bot-regex* (tooter:note account))
       (some #'identity (loop for (f . v) in (tooter:fields account)
 			     collect (or (cl-ppcre:scan *no-bot-regex* f)
@@ -25,13 +25,26 @@ if INCLUDE-MENTIONS is non-nil, include mentions besides the primary account bei
   (let* ((client (bot-client *bot*))
 	 (reply-account (tooter:find-account client (tooter:in-reply-to-account-id status)))
 	 (reply-mentions (loop for mention in (tooter:mentions status)
-			    unless (string= (tooter::account-name mention)
-					    (tooter::account-name reply-account))
-			    collect (concatenate 'string "@" (tooter::account-name mention)))))
+			    unless (string= (tooter:username mention)
+					    (tooter:username reply-account))
+			    collect (concatenate 'string "@" (tooter:username mention)))))
     (tooter:make-status client (str:join " "
-					 `(,(tooter:account-name reply-account)
+					 `(,(tooter:username reply-account)
 					   ,@(when include-mentions reply-mentions)
 					   ,text))
 			:visibility (tooter:visibility status)
 			:spoiler-text (tooter:spoiler-text status)
 			:in-reply-to (tooter:id status))))
+
+(defun post (text &key (visibility :unlisted) cw sensitive media)
+  "a thin wrapper around tooter:make-status
+
+see documentation for that function"
+  (tooter:make-status (bot-client *bot*)
+		      text
+		      :visibility visibility
+		      :spoiler-text cw
+		      :media media
+		      :sensitive sensitive))
+
+		  
