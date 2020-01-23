cb967733a7836094c10848c02584dfbaac736ba8
ava <dev@computerfox.xyz>
2020-01-23 16:24:12 -0500

    changed run-bot to a macro
    removed global *websocket-client* var
    fixed dispatch not using correct agetf indicators
    fixed dispatch causing crashes because bots slot not being bound

--- glacier.lisp
+++ glacier.lisp
@@ -3,44 +3,49 @@
 (in-package #:glacier)
 
 (defvar *bot*)
-(defvar *websocket-client*)
 
-(defmethod run-bot ((bot mastodon-bot))
-  (handler-case
-      (with-user-abort
-	(let ((*bot* bot)
-	      (*websocket-client* (wsd:make-client (format nil "~a/api/v1/streaming?access_token=~a&stream=~a"
-							   (get-mastodon-streaming-url)
-							   (config :mastodon-token)
-							   (config :timeline "user")))))
-	  (wsd:on :open *websocket-client* #'print-open)
-	  (wsd:on :message *websocket-client* #'dispatch)
-	  (wsd:on :close *websocket-client* #'print-close)
-	  (wsd:start-connection *websocket-client*)
+(defmacro run-bot (bot &body body)
+  "runs BOT, setting up websocket handlers and starting the streaming connection before executing BODY
+
+if BODY is not provided drops into a loop where we sleep until the user quits us, or our connection closes"
+  `(handler-case
+       (with-user-abort
+	 (setf *bot* ,bot)
+	 (let ((*websocket-client* (wsd:make-client (format nil "~a/api/v1/streaming?access_token=~a&stream=~a"
+							    (get-mastodon-streaming-url)
+							    (config :mastodon-token)
+							    (config :timeline "user")))))
+	   (wsd:on :open *websocket-client* #'print-open)
+	   (wsd:on :message *websocket-client* #'dispatch)
+	   (wsd:on :close *websocket-client* #'print-close)
+	   (wsd:start-connection *websocket-client*)
 	
-	  (loop while (eq (wsd:ready-state *websocket-client*) :open)
-	        do (sleep 5))))
-    (user-abort ()
-      (format t "shutting down~%"))
-    (error (e)
-      (format t "encountered unexpected error: ~A~%" e))))
+	   ,@(if body
+		 body
+		 '((loop do (sleep 5)
+		        while (eq (wsd:ready-state *websocket-client*) :open))))))
+     (user-abort ()
+       (format t "shutting down~%"))
+     (error (e)
+       (format t "encountered unexpected error: ~A~%" e))))
 
 (defun dispatch (message)
+  "gets the type of MESSAGE we intercepted and calls the appropriate functions on our bot with the proper tooter object"
   (let* ((parsed (json:decode-json-from-string message))
-	 (parsed-payload (json:decode-json-from-string (agetf parsed "payload"))))
+	 (parsed-payload (json:decode-json-from-string (agetf parsed :payload))))
     
     (cond
-      ((and (string= (agetf parsed "event") "update")
-	    (bot-on-update *bot*))
+      ((and (string= (agetf parsed :event) "update")
+	    (slot-boundp *bot* 'on-update))
        (funcall (bot-on-update *bot*) 
 		(tooter:find-status (bot-client *bot*) (agetf parsed-payload :id))))
       
-      ((and (string= (agetf parsed "event") "delete")
-	    (bot-on-delete *bot*))
+      ((and (string= (agetf parsed :event) "delete")
+	    (slot-boundp *bot* 'on-delete))
        (funcall (bot-on-delete *bot*) parsed-payload))
       
-      ((and (string= (agetf parsed "event") "notification")
-	    (bot-on-notification *bot*))
+      ((and (string= (agetf parsed :event) "notification")
+	    (slot-boundp *bot* 'on-notification))
        (funcall (bot-on-notification *bot*) 
 		(tooter:find-notification (bot-client *bot*) (agetf parsed-payload :id))))
 
